8f6fb2b4936fb923fbb1064ac190d1d9a652740f
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-23 17:47:08 +0300

    //

--- crud/forms.py
+++ crud/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+
 class UserForm(forms.Form):
-    name = forms.CharField(label="Ваше имя")
-    age = forms.IntegerField(label="Ваш возраст")
+    name = forms.CharField(
+        label='Имя',
+        initial='undefined',
+        help_text='Введите свое имя')
+    age = forms.IntegerField(
+        label='Ваш возраст?',
+        initial=18,
+        help_text='Введите свой возраст')
