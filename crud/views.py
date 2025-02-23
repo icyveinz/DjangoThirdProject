7a07ea793e5451f6209f39389c429e51927a78dd
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-23 17:17:13 +0300

    //

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from crud.forms import UserForm
+
 
 # Create your views here.
+
+def index(request):
+    userform = UserForm()
+    return render(request, 'index.html', {'form': userform})
