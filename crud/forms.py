7be815aee728f80ea370b52328c2411a2023f59f
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-24 15:19:04 +0300

    //

--- crud/forms.py
+++ crud/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class UserForm(forms.Form):
     name = forms.CharField(label='Имя', help_text='Введите свое имя', min_length=2, max_length=10)
-    age = forms.IntegerField(label='Ваш возраст?', help_text='Введите свой возраст')
+    age = forms.IntegerField(min_value=1, max_value=100)
+    weight = forms.DecimalField(min_value=3, max_value=200, decimal_places=2)
     reklama = forms.BooleanField(label='Coглacны получать рекламу?', required=False)
--- crud/views.py
+++ crud/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .forms import UserForm
 
 
 def index(request):
     if request.method == 'POST':
-        name = request.POST.get('name') # получить значение поля Имя
-        age = request.POST.get('age') # получить значение поля Возраст
-        return render(request,'reply.html',{"content" : {"name":name, "age":age}})
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data['name']  # получить значение поля Имя
+            age = userform.cleaned_data['age']  # получить значение поля Возраст
+            return render(
+                request,
+                'reply.html',
+                {"content": {"name": name, "age": age}})
+        else:
+            return render(
+                request,
+                'reply.html',
+                {"content": {"name": "You haven't passed the validation", "age": ""}})
     else:
         userform = UserForm()
         return render(request, 'index.html', {'form': userform})
